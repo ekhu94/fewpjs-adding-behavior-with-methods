b53e22d9044dc18c5eba7494503fcfc7830ff39f
Erik Huang <erikhuangdrums@gmail.com>
2021-03-05 18:40:25 -0800

    commit tree b1c58fefea07d560e45dd6faa746bf857cbd24b5

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =sex;
+    }
+    speak() {
+        const {name, sex} = this;
+        return sex === 'male' ? `It's me! ${name}, the parrot!` : `${name} says squawk!`;
+    }
+}
